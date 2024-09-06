d60c832435148f8be48abad4a7f3b861f2398715
magisterblaman <hannes.blaman@lbs.se>
2024-09-06 09:37:33 +0200

    Inkapsling med metoder

---
+++ SY22_PRRPRR02_07_Inkapsling/BankAccount.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SY22_PRRPRR02_07_Inkapsling {
+	internal class BankAccount {
+		private decimal _balance;
+		private string _accountNumber;
+		private string _holderName;
+
+		public decimal GetBalance() {
+			return _balance;
+		}
+		protected void SetBalance(decimal newBalance) {
+			if (newBalance >= 0) {
+				_balance = newBalance;
+			}
+		}
+
+		public string GetAccountNumber() {
+			return _accountNumber;
+		}
+		private void SetAccountNumber(string newAccountNumber) {
+			if (newAccountNumber != "") {
+				_accountNumber = newAccountNumber;
+			}
+		}
+
+		// getter
+		public string GetHolderName() {
+			return _holderName;
+		}
+		// setter
+		public void SetHolderName(string newHolderName) {
+			if (newHolderName != "") {
+				_holderName = newHolderName;
+			}
+		}
+
+		public BankAccount(string inputHolderName,
+			string inputAccountNumber,
+			decimal initialBalance) {
+
+			SetBalance(initialBalance);
+			SetAccountNumber(inputAccountNumber);
+			SetHolderName(inputHolderName);
+		}
+	}
+}
--- SY22_PRRPRR02_07_Inkapsling/Program.cs
+++ SY22_PRRPRR02_07_Inkapsling/Program.cs
@@ -7,6 +7,21 @@ using System.Threading.Tasks;
 namespace SY22_PRRPRR02_07_Inkapsling {
 	internal class Program {
 		static void Main(string[] args) {
+			BankAccount account = new BankAccount("Hannes Blåman", "437957487485", 100);
+
+			Console.WriteLine("Kontot för "
+				 + account.GetHolderName() + " (kontonr: "
+				 + account.GetAccountNumber()
+				 + ") har " + account.GetBalance() + "kr");
+
+			account.SetHolderName("Per Blåman");
+
+			Console.WriteLine("Kontot för "
+				 + account.GetHolderName() + " (kontonr: "
+				 + account.GetAccountNumber()
+				 + ") har " + account.GetBalance() + "kr");
+
+			
 		}
 	}
 }
